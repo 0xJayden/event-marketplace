9219ca28f79e3821d63e29e45642bb3df9b85e82
0xJayden <gannixcrypto@protonmail.com>
2022-08-14 20:30:16 -0700

    updated url

--- components/get-event-data.tsx
+++ components/get-event-data.tsx
@@ -2,10 +2,7 @@ import useSWR from "swr";
 
 export default function GetEventData() {
   const fetcher = (url: string) => fetch(url).then((res) => res.json());
-  const { data, error } = useSWR(
-    "https://event-marketplace.vercel.app/api/get-events",
-    fetcher
-  );
+  const { data, error } = useSWR("/api/get-events", fetcher);
 
   return {
     data,
