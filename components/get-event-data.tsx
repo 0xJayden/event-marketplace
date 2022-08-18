4d3465b7e059141ab12f30afdee9ac9473b723a7
0xJayden <gannixcrypto@protonmail.com>
2022-08-18 14:16:31 -0700

    added revalidation interval

--- components/get-event-data.tsx
+++ components/get-event-data.tsx
@@ -2,7 +2,9 @@ import useSWR from "swr";
 
 export default function GetEventData() {
   const fetcher = (url: string) => fetch(url).then((res) => res.json());
-  const { data, error } = useSWR("/api/get-events", fetcher);
+  const { data, error } = useSWR("/api/get-events", fetcher, {
+    refreshInterval: 1000,
+  });
 
   return {
     data,
